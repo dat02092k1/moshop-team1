1f14a1e630d9a050a0c8367d739c0a31173c7586
dothuongton <76248906+dothuongton@users.noreply.github.com>
2022-08-10 14:50:10 +0700

    update branch ton

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router"; 
+// import { RouterLink, RouterView } from "vue-router";
+// import ListMain from "./layouts/main/pages/ListMain.vue";
+// import LoginMain from "./layouts/main/pages/LoginMain.vue";
+import AddMain from "./layouts/main/pages/AddMain.vue";
 </script>
 
 <template>
-  
-  <RouterView />
+  <div>
+    <!--    <ListMain />-->
+    <!--        <LoginMain />-->
+    <AddMain />
+  </div>
+
+  <!--  <RouterView />-->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
